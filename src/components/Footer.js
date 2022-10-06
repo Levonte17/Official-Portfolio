f9b726cdd26506808b66f9c347834127d3e794e1
Levonte Larry <levontelarry@Levontes-MacBook-Air.local>
2022-10-06 11:29:38 -0400

    scss, header, footer, dashboard, home

---
+++ src/components/Footer.js
@@ -0,0 +1,6 @@
+function Footer(props) {
+    
+    return<h1>Copyrights</h1>;
+}
+
+export default Footer;
---
+++ src/pages/Home.js
@@ -0,0 +1,6 @@
+function Home(props){
+
+    return<h1>The Digital Expert</h1>;
+}
+
+export default Home;
