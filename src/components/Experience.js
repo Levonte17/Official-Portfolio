99ee37723d13ed17fcafcee735518dc1a9fbcd7a
Levonte Larry <you@example.com>
2022-10-29 09:57:59 -0400

    experience update

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,9 +1,19 @@
+import bgvid1  from '../assets/website.mp4';
+import one from '../assets/second.jpeg';
 
 function Experience(props) {
     return(
-        <h1>Experience</h1>
+        <div className="image">
             
-    )
-};
+            <h1>
+                <img src={second} />
+            </h1>
+            <h1>
+                <video src={website} alt="about" autoPlay loop muted />
+            </h1>
+        </div>
+            
+    );
+}
 
 export default Experience;
