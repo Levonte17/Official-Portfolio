be9607360a77a650316c237753c5fa9bf391107f
Levonte Larry <you@example.com>
2023-01-19 21:05:20 -0500

    mobile websites

--- src/pages/Websites.js
+++ src/pages/Websites.js
@@ -1,9 +1,8 @@
 
-import samp from '../assets/samp.gif';
-import barber from '../assets/barber.gif';
-import e from '../assets/e.gif';
-import colty from '../assets/colty.gif';
-import humble from '../assets/humble.gif';
+import samp from '../assets/samp.png';
+import barber from '../assets/barber.png';
+import ekust from '../assets/ekust.png';
+import colty from '../assets/colty.png';
 import React from 'react';
 
 function Websites(props) {
@@ -12,42 +11,108 @@ function Websites(props) {
     <h1>
         Websites And Features
     </h1>
-            
+
+
+<div className='web2'>
+    <img alt='imgw' src={samp} />
+
+    <h2>
+        SAMP FITNESS 
+    </h2>
+
+    <p>
+        I collaborated with the owner on SAMP Fitness 
+        to provide graphic designs to support his brand vision 
+        and personality.
+        This website is easy to join by simply loging in with
+        your preexisting google account to ensure extra security
+        and privacy of your data.
+    </p>
             <a className='img' href="https://sampfitness.netlify.app">
-                <img alt='imgw' src={samp} />
+                <button> 
+                    Visit Website 
+                </button>
             </a>
-            <p>
-                
-            </p>
+            <br/>
+            <br/>
+</div>  
+
+           
             
-            <a className='img' href="https://brushcutz.netlify.app">
-                <img alt='imgw' src={barber} />
+<div className='web2'>
+    <img alt='imgw' src={barber} />
+
+    <h2>
+        BRUSHCUTZ BARBERSHOP 
+    </h2>
+
+    <p>
+        I collaborated with the owner on SAMP Fitness 
+        to provide graphic designs to support his brand vision 
+        and personality.
+        This website is easy to join by simply loging in with
+        your preexisting google account to ensure extra security
+        and privacy of your data.
+    </p>
+               <a className='img' href="https://brushcutz.netlify.app">
+                <button> 
+                    Visit Website 
+                </button>
             </a>
-            <p>
+            <br/>
+            <br/>
+</div> 
 
-            </p>
 
-            <a className='img' href="https://humbleme.netlify.app">
-                <img alt='imgw' src={humble} />
+<div className='web2'>
+    <img alt='imgw' src={ekust} />
+
+    <h2>
+        E-KUSTOMS LLC 
+    </h2>
+
+    <p>
+        I collaborated with the owner on SAMP Fitness 
+        to provide graphic designs to support his brand vision 
+        and personality.
+        This website is easy to join by simply loging in with
+        your preexisting google account to ensure extra security
+        and privacy of your data.
+    </p>
+        <a className='img' href="https://ekustoms.adaptable.app/">
+            <button> 
+                Visit Website 
+            </button>
+        </a>
+            <br/>
+            <br/>
+</div>
+
+
+
+<div className='web2'>
+    <img alt='imgw' src={colty} />
+
+    <h1>
+        COLTY TEMPLE NCOC
+    </h1>
+
+    <p>
+        I collaborated with the owner on SAMP Fitness 
+        to provide graphic designs to support his brand vision 
+        and personality.
+        This website is easy to join by simply loging in with
+        your preexisting google account to ensure extra security
+        and privacy of your data.
+    </p>
+               <a className='img' href="https://coltytemple.netlify.app">
+                <button> 
+                    Visit Website 
+                </button>
             </a>
-            <p>
-
-            </p>
-
-            <a className='img' href="https://ekustoms.adaptable.app/">
-                <img alt='imgw' src={e} />
-            </a>
-            <p>
-                
-            </p>
-
-
-            <a className='img' href="https://humbleme.netlify.app">
-                <img alt='imgw' src={colty} />
-            </a>
-            <p>
-                
-            </p>
+            <br/>
+            <br/>
+</div>
     </div>
     );
 }
