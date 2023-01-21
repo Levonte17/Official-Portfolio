35bd50bbcbada5c49b8dcfbfe709cd64fdb8f873
Levonte Larry <you@example.com>
2023-01-20 20:32:11 -0500

    podcast and business

--- src/pages/Business.js
+++ src/pages/Business.js
@@ -1,19 +1,29 @@
 import React from 'react'
+import bthree from '../assets/bthree.jpg'
+import btwo from '../assets/btwo.jpg'
+import bone from '../assets/bone.jpg'
 
 function Business(props) {
   return (
     <div className='business'>
+    <img  className='business' src={ btwo } alt='me' />
+    <img  className='business' src={ bthree } alt='me' />
+    <img  className='business' src={ bone } alt='me' />
       <h1>
         Organize Your Business
       </h1>
       <p> 
 
-Create your business email ( yourName@yourSite.com ) to match your domain name and easily access your site analytics to assist in growing your brand and receiving advertisement. 
-
-Host your own video conferences with clients and presentations to better organize your communication with clients. 
-
-Logos and business cards component
+Create a business email ( yourName@yourWebsite.com ) to match your 
+domain name and easily access your site analytics to assist in 
+growing your brand and building the most effective business 
+solutions for your brand. 
+<br/>
+Host your own video conferences with clients and present your client 
+presentations to better organize your communication with clients. 
       </p>
+    
+
     </div>
   )
 }
--- src/pages/Pod.js
+++ src/pages/Pod.js
@@ -3,15 +3,17 @@ import me from '../assets/me.png'
 
 function Pod(props) {
   return (
-    <div className='podcat'>
+    <div className='podcast'>
       <h1>
         INSPERATIONAL STORIES
       </h1>
       <p>
-    Get featured for our most blogs or podcasts of inspiring stories where we spotlight your success and accomplishments as well as your aspiring accomplishments for your future.
-
-Personalize your blogs by adding your own YourName.com. With this FREE domain name your name gains Google Analytics and you can easily display your personal services and become accessible. 
-
+      Share your inspiring stories. Spotlight your success and current accomplishments as well as your aspiring goals for the future.
+<br/>
+     Personalize your blogs by adding your own YourName.com
+    <br/>With this FREE domain name your name gains Google Analytics and you can 
+    easily display your personal services and become easily accessible. 
+<br/>
  Popular for sales representatives and agents as they are forced to communicate less but advertise their services more with easy access
 
       </p>
