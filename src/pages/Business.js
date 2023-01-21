9baf34324ab9659946536247ae82de346d310f21
Levonte Larry <you@example.com>
2023-01-21 08:00:14 -0500

    css

--- src/pages/Business.js
+++ src/pages/Business.js
@@ -2,6 +2,7 @@ import React from 'react'
 import bthree from '../assets/bthree.jpg'
 import btwo from '../assets/btwo.jpg'
 import bone from '../assets/bone.jpg'
+import customize from '../assets/customize.png';
 
 function Business(props) {
   return (
@@ -14,16 +15,20 @@ function Business(props) {
       </h1>
       <p> 
 
-Create a business email ( yourName@yourWebsite.com ) to match your 
+Create a business email (yourName@yourWebsite.com) to match your 
 domain name and easily access your site analytics to assist in 
 growing your brand and building the most effective business 
 solutions for your brand. 
 <br/>
 Host your own video conferences with clients and present your client 
 presentations to better organize your communication with clients. 
-      </p>
-    
-
+  </p>
+  <img className="webimg" src={customize} alt="loca"/>
+  <br/>
+  <br/>
+<button>
+    Get Started
+</button>
     </div>
   )
 }
