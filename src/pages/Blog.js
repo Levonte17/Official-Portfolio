fabea816c52273b428645e9655d157a135cf6370
Levonte Larry <you@example.com>
2023-03-30 20:13:10 -0400

    mobile styling

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import otcb from '../assets/otcb.png';
+import otc from '../assets/otc.png';
 import mai from '../assets/mai.png';
 function Blog(props) {
   return (
     <div className='blogs'>
         <h1> Digital Investors Blogs</h1>
       <Link to='/kodyblog' >
-        <img src={otcb} alt='kody' className="jamori" /> 
+        <img src={otc} alt='kody' className="jamori" /> 
         <h2>Off The Curb Apparel</h2>
       </Link>
       <Link to='/isoblog' >
